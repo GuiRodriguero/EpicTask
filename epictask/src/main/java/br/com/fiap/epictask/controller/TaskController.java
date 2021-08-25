1c2237241c00946a68af65684eac9671a60def48
Guilherme Rodriguero <gui.rodriguero@hotmail.com>
2021-08-25 03:10:03 -0300

    Aula 3 concluída - Validation

--- epictask/src/main/java/br/com/fiap/epictask/controller/TaskController.java
+++ epictask/src/main/java/br/com/fiap/epictask/controller/TaskController.java
@@ -1,10 +1,16 @@
 package br.com.fiap.epictask.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.fiap.epictask.model.Task;
 import br.com.fiap.epictask.repository.TaskRepository;
@@ -17,22 +23,26 @@ public class TaskController {
 	
 	//@RequestMapping(value="/task", method=RequestMethod.GET)
 	@GetMapping("/task")
-	public String index() {//Método que mostra a tela das tarefas
+	public ModelAndView index() {//Método que mostra a tela das tarefas
 		//return "/tasks.html";
-		return "/tasks";
+		ModelAndView modelAndView = new ModelAndView("tasks");
+		List<Task> tasks = repository.findAll();
+		modelAndView.addObject("tasks", tasks);
+		return modelAndView;
 	}
 	
 	@RequestMapping("/task/new")
 	public String create() {//Método que mostra a tela de criação de tarefas
-		return "/task-form";
+		return "task-form";
 	}
 	
 	//@RequestMapping(value="/task", method=RequestMethod.POST)
 	@PostMapping("/task")
-	public String save(Task task) {
+	public String save(@Valid Task task, BindingResult result) {
+		if(result.hasErrors()) {
+			return "tasks-form";
+		}
 		repository.save(task);
-		repository.findByTitle(task.getTitle());
-		System.out.println("Salvando tarefa");
 		return "tasks";
 	}
 }
--- epictask/src/main/java/br/com/fiap/epictask/model/Task.java
+++ epictask/src/main/java/br/com/fiap/epictask/model/Task.java
@@ -4,18 +4,27 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
 
 @Data
 @Entity
 public class Task {
-	
+
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@NotBlank(message = "O título é obrigatório")
 	private String title;
+	
+	@Size(min=10, message = "Deve ter pelo menos 10 caracteres")
 	private String description;
-	private int points;
 	
-	
+	@Min(value=10, message = "A pontuação mínima é 10")
+	@Max(value=500, message = "A pontuação máxima é 500")
+	private int points; 
 }
